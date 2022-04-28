0684fdd5260bb67b2836ee15203fa0b1345774f7
NanoCode012 <kevinvong@rocketmail.com>
2022-04-28 16:43:43 +0700

    Remove useCallback

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {useData, useTheme, useTranslation} from '../hooks/';
 import {useNavigation} from '@react-navigation/native';
